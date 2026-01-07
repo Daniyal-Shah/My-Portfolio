1d3c89ead635c47145a6bb9a594daffff11d1cad
Daniyal-Shah <51289334+Daniyal-Shah@users.noreply.github.com>
2026-01-07 14:29:40 +0500

    Fixed issue of content not showing properly

--- src/app/content/content.ts
+++ src/app/content/content.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PortfolioNavbar } from '../portfolio-navbar/portfolio-navbar';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-content',
-  imports: [RouterOutlet, PortfolioNavbar],
+  imports: [RouterOutlet, PortfolioNavbar, Sidebar],
   templateUrl: './content.html',
   styleUrl: './content.css',
 })
